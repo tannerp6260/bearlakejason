4852b64a9047c59b9f213b23a84bdcb0220dd4e8
Tanner Petersen <68283708+tannerp6260@users.noreply.github.com>
2026-05-09 11:07:33 -0600

    Fix Tailwind styling build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://tannerp6260.github.io',
   base: '/bearlakejason',
+  integrations: [tailwind({ applyBaseStyles: false })],
 });
