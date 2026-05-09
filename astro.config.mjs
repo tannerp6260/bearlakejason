d1cafea977c98eedfc307d5258adb6b1db8644c1
Tanner Petersen <68283708+tannerp6260@users.noreply.github.com>
2026-05-09 07:01:38 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tannerp6260.github.io',
-  base: '/bearlake-jason',
+  base: '/bearlakejason',
 });
