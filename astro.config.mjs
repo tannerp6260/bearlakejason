65f449cc7fe708a21e80a8c0932553fceaccbdaa
tanner petersen <tannerp6260@gmail.com>
2026-05-09 18:39:31 -0600

    updated astro.config.mjs to point to cloudflare dev url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bearlakejason.com',
+	site: 'https://bearlakejason.pages.dev',
   integrations: [tailwind(), sitemap()],
 });
